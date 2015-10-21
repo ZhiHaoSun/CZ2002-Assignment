fc713d0fd1b04529b79b3a86e093751644b95ccf
Sun Zhi Hao <ZSUN005@e.ntu.edu.sg>
2015-10-21 13:40:12 +0800

    Completion of Moive and Model classes
    I made a new version of model operation.

--- MOBLIMA/src/com/moblima/project/view/Moblima.java
+++ MOBLIMA/src/com/moblima/project/view/Moblima.java
@@ -1,22 +1,36 @@
 package com.moblima.project.view;
 
+import java.io.IOException;
 import java.util.Scanner;
 
-import com.moblima.project.controller.AccountManager;
-import com.moblima.project.controller.MovieManager;
+import org.json.JSONException;
+
 import com.moblima.project.model.Movie;
-import com.moblima.project.model.Movie.Language;
-import com.moblima.project.model.Movie.Rating;
-import com.moblima.project.model.Movie.Status;
-import com.moblima.project.model.User;
 
 public class Moblima {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) throws IOException, JSONException {	
+		
+		Movie.load();
+		
 		Scanner sc = new Scanner(System.in);
 		sc.useDelimiter("\\n");
-		
+                
 		MainMenu main = new MainMenu(sc);
-		main.displayMenu();
+		
+//		JSONObject object = new JSONObject();
+//		object.put("id", 2);
+//		object.put("title", "Avengers");
+//		object.put("synopsis", "abcde");
+//		object.put("director","Sean");
+//		object.put("status","COMING_SOON");
+//		object.put("language","THAI");
+//		object.put("rating","NO");
+//		object.put("casts",new JSONArray());
+//		
+//		Movie movie = new Movie(object);
+//		movie.save();
+//		main.displayMenu();
+		main.displayMovies();
 	}
 }
